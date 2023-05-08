1fa722bb2137f851315d045c452cdb3657a31ead
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-05-08 12:27:08 +0530

    first commit

---
+++ onefinance_custom_chatbot/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for onefinance_custom_chatbot project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onefinance_custom_chatbot.settings')
+
+application = get_wsgi_application()
