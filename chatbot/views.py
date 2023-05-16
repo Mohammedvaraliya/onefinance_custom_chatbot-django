d578c7ae8bc5f66afc00a0a7af668e7ea7a9de4b
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-05-16 16:50:35 +0530

    Update UI

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 from .chatbot_logic import get_response
-from django.http import JsonResponse
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def query(request):
-    if request.method == 'POST':
-        query = request.POST.get('query', '')
-        if not query:
-            response = "Please enter something"
-        else:
-            response = get_response(query)
-        data = {'query': query, 'response': response}
-        return JsonResponse(data)
+    now = datetime.now()
+    if now.hour < 12:
+        greeting = "Good morning"
+    elif now.hour < 18:
+        greeting = "Good afternoon"
     else:
-        return render(request, 'query.html')
+        greeting = "Good evening"
+
+    if request.method == 'POST':
+        query = request.POST.get('input_text', '')
+        response = get_response(query)
+        return HttpResponse(response)
+    else:
+        context = {
+            'greeting': greeting
+        }
+        return render(request, 'query.html', context)
